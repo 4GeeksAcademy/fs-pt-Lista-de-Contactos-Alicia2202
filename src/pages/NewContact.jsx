71247da05fc976beb180bec7a33d3295d0361d32
Alicia2202 <alicialf02@gmail.com>
2026-03-28 17:45:23 +0000

    Styles: Añadido estilos en la pagina New Contact y el link para volver a home

--- src/pages/NewContact.jsx
+++ src/pages/NewContact.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 const NewContact = () => {
     const [formData, setFormData] = useState({
         name: '',
@@ -7,22 +8,40 @@ const NewContact = () => {
         address: ''
     })
     const handleChange = e => {
-        setFormData({ ...formData, [e.target.name]:e.target.value})
-        
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+
     }
     const handleSubmit = e => {
         e.preventDefault()
     }
     return (
         <div>
-            <h1>New Contact</h1>
-            <form onSubmit={handleSubmit}>
-                <input className="form-control" type="text" name="name" value={formData.name} onChange={handleChange } />
-                <input className="form-control" type="text" name="phone" value={formData.phone} onChange={handleChange} />
-                <input className="form-control" type="text" name="email" value={formData.email} onChange={handleChange } />
-                <input className="form-control" type="text" name="address" value={formData.address} onChange={handleChange } />
-                <input type="submit" />
-            </form>
+            <div className="container mt-5">
+                <h1 className="text-center">Add a new contact</h1>
+
+                <form onSubmit={handleSubmit}>
+                    <div className="className mb-3">
+                        <label htmlFor="name" className="form-label fw-bold">Full Name</label>
+                        <input className="form-control" type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+                    </div>
+                    <div className="className mb-3">
+                        <label htmlFor="email" className="form-label fw-bold">Email</label>
+                        <input className="form-control" type="text" id="email" name="phone" value={formData.phone} onChange={handleChange} />
+                    </div>
+                    <div className="className mb-3">
+                        <label htmlFor="phone" className="form-label fw-bold">Phone</label>
+                        <input className="form-control" type="text" id="phone" name="email" value={formData.email} onChange={handleChange} />
+                    </div>
+                    <div className="className mb-3">
+                        <label htmlFor="address" className="form-label fw-bold">Address</label>
+                        <input className="form-control" type="text" id="address" name="address" value={formData.address} onChange={handleChange} />
+                    </div>
+                    <button type="submit" className="btn btn-primary w-100">save</button>
+                </form>
+                <div className="mt-3">
+                    <Link to="/">or get back to contacts</Link>
+                </div>
+            </div>
         </div>
     )
 }
