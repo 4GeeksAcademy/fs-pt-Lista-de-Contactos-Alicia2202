fc506aa4211a6dff15c14902220e173b7a2c5ca8
Alicia2202 <alicialf02@gmail.com>
2026-03-30 11:16:41 +0000

    Styles:Agregado el estilo de la card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,41 @@
 
-const Card = ({name, phone, mail}) => {
+const Card = ({name, phone, mail, address}) => {
     return (
-        <div className="card">
-            <p>{name}</p>
-            <p>{phone}</p>
-            <p>{mail}</p>
+
+        <div className="list-group-item p-4">
+            <div className="row align-items-center">
+                <div className="col-md-2 d-flex justify-content-center">
+                    <img 
+                        src="https://picsum.photos/200" 
+                        alt="contact" 
+                        className="rounded-circle image-contact"
+                        
+                    />
+                </div>
+
+            <div className="col-md-8">
+                    <h5 className="mb-3">{name}</h5>
+                    <p className="text-secondary mb-1">
+                        <i className="fas fa-map-marker-alt me-3"></i>{address || "No address"}
+                    </p>
+                    <p className="text-secondary mb-1">
+                        <i className="fas fa-phone me-3"></i>{phone}
+                    </p>
+                    <p className="text-secondary mb-0">
+                        <i className="fas fa-envelope me-3"></i>{mail}
+                    </p>
+                </div>
+
+                <div className="col-md-2 text-end d-flex justify-content-end align-self-start">
+                    <button className="btn btn-link text-dark p-2">
+                        <i className="fas fa-pencil-alt"></i>
+                    </button>
+                    <button className="btn btn-link text-dark p-2" >
+                        <i className="fas fa-trash-alt"></i>
+                    </button>
+                </div>
+           
+            </div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,34 @@
 import contactApi from "../../services/contactAPI.js";
 import Card from "../components/Card.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
-  useEffect(()=>{
-	contactApi.getAgenda().then(data => dispatch({
-		type:'updateContactData',
-		payload:{
-			data
-		}
-	}))
-  },[])
+	useEffect(() => {
+		contactApi.getAgenda().then(data => dispatch({
+			type: 'updateContactData',
+			payload: {
+				data
+			}
+		}))
+	}, [])
 
-  console.log("Contenido de store.contactsData",store.contactsData);
+	console.log("Contenido de store.contactsData", store.contactsData);
 	return (
-		<div className="text-center mt-5">
+		<div className="container mt-5">
+			<div className="d-flex justify-content-end mb-3">
 			<Link className="btn btn-primary" to={'/create'}>Create Contact</Link>
+			</div>
+			<div className="border shadow-sm rounded">
 			{
-				store.contactsData.contacts?.map(el=> <Card key={el.id} name={el.name} phone={el.phone} mail={el.email}/>)
+				store.contactsData.contacts?.map(el=> <Card key={el.id} name={el.name} phone={el.phone} mail={el.email} address={el.address}/>)
 			}
 		</div>
-		
+		</div>
+
 	);
 }; 
